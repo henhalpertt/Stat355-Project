acff4a5c74c76da79b5476b75500e12c03e13215
Hen Halpert <henhhalpert@gmail.com>
2021-04-07 23:17:00 -0700

    adding plots for report

--- draft.R
+++ draft.R
@@ -1,21 +1,23 @@
 '''
-Question we want to answer ? 
-
 AGE(int) : Patient age 
 
 FEMALE(int) : Female / Other - categorical  
 
 RACE(int) : Race [1,...6] - categorical 
 
-APRDRG(int) : ? Groupings maybe - categorical 
+APRDRG(int) : Groupings  - categorical (for example, 640 - infants group)
 
 LOS(int) : Length of stay [ probably days] - numerical
 
 TOTCHG(num) : Total Charge to the hospital  - numerical 
 '''
-
+install.packages("viridis")  
+install.packages("robust")  
 library(corrplot)
 library(ggplot2)
+library(viridis)
+library(robust)
+
 setwd('~/Desktop/Stat355/Final-Project/')
 data <- read.csv("HospitalCosts.csv")
 ## Preprocess ##
@@ -27,19 +29,22 @@ str(data)
 data.matrix <- as.matrix(data)
 length(data.matrix)
 corrplot(cor(data), type = "upper", "number")
-
-
+?corrplot
 '1. wants to find the age category
 of people who frequent the hospital and has the maximum expenditure.
+
+imbalanced data: how many babies compared to other age groups ? 
+xtabs(~AGE,data=data)s - 306 individuals are babies.
 '
+
 # Total charges by age
 AGE.groups <- seq(0,17)
-age.TOTCHG <- ggplot(data, aes(AGE, TOTCHG, fill=TOTCHG)) +
-           geom_bar(stat="identity") +
-           scale_x_continuous("AGE", labels = as.character(AGE.groups), breaks = AGE.groups)
+age.TOTCHG <- ggplot(data, aes(AGE, TOTCHG, fill=AGE)) +
+  geom_bar(stat="identity") +
+  scale_x_continuous("AGE", labels = as.character(AGE.groups), breaks = AGE.groups)
 age.TOTCHG
 
-age.LOS <- ggplot(data, aes(AGE, LOS, fill=LOS)) +
+age.LOS <- ggplot(data, aes(AGE, LOS, fill=AGE)) +
   geom_bar(stat="identity") +
   scale_x_continuous("AGE", labels = as.character(AGE.groups), breaks = AGE.groups)
 age.LOS
@@ -70,14 +75,15 @@ group.LOS
 #xtabs(~APRDRG,data=data)
 
 '3.needs to analyze if
-the race of the patient is related to the hospitalization costs.
+the race[cat]of the patient is related to the hospitalization costs[num].
 '
 
 plot(data$RACE, data$TOTCHG, pch=19) # no visible trend 
 
-
+RACE.groups <- seq(1,6)
 RACE.TOTCHG <- ggplot(data, aes(RACE, TOTCHG, fill=RACE)) +
-  geom_bar(stat="identity") 
+  geom_bar(stat="identity", show.legend = F) +
+  scale_x_continuous("RACE", labels = as.character(RACE.groups), breaks = RACE.groups)
 RACE.TOTCHG
 
 
@@ -86,12 +92,10 @@ RACE.TOTCHG <- ggplot(data, aes(AGE, TOTCHG, fill=RACE.factored)) +
   geom_bar(stat="identity",position=position_dodge()) 
 RACE.TOTCHG
 
+
 '4. has to analyze the severity of the
 hospital costs by age and gender for proper allocation of resources.
 '
-install.packages("viridis")  
-library("viridis")
-
 
 str(data)
 pal <- wes_palette("Zissou1", 100, type = "continuous")
@@ -104,7 +108,9 @@ ggplot(data, aes(factor(AGE), TOTCHG)) +
 FEMALE.factored <- ifelse(data$FEMALE == 1, 'FEMALE', 'NOT FEMALE' )
 
 ggplot(data, aes(factor(FEMALE.factored), TOTCHG)) + 
-  geom_boxplot()
+  geom_boxplot() + 
+  coord_flip()
+
 
 ggplot(data, aes(factor(FEMALE.factored), TOTCHG)) + 
   geom_bar(stat='identity',fill = "#ff6668") +
@@ -113,7 +119,6 @@ ggplot(data, aes(factor(FEMALE.factored), TOTCHG)) +
 model1 <- lm(TOTCHG ~ AGE + FEMALE, data=data)
 summary(model1)
 
-# F-statistic: 6.581 on 2 and 496 DF,  p-value: 0.001511
 
 '5. the agency wants to find if the length of stay can be predicted
 from age, gender, and race.
@@ -129,4 +134,72 @@ mainly affects the hospital costs.
 model3 <- lm(TOTCHG ~ ., data=data)
 summary(model3)
 
+
+model4 <- lm(TOTCHG ~ AGE + LOS + APRDRG, data=data)
+summary(model4)
+
+'AGE - 
+TOTCHG - 
+LOS - '
+
+'Adjusted R-squared:  0.5479 '
+
+
+''
+##################################################################
+##########################   For Report    #######################
+##################################################################
+require(gridExtra)
+
+AGE.groups <- seq(0,17)
+age.TOTCHG <- ggplot(data, aes(AGE, TOTCHG, fill=AGE)) +
+  geom_bar(stat="identity", show.legend = F) +
+  scale_x_continuous("AGE", labels = as.character(AGE.groups), breaks = AGE.groups) +
+  labs(y = "Total Charges") +
+  age.TOTCHG
+
+age.LOS <- ggplot(data, aes(AGE, LOS, fill=AGE)) +
+  geom_bar(stat="identity") +
+  scale_x_continuous("AGE", labels = as.character(AGE.groups), breaks = AGE.groups) +
+  scale_fill_binned(name = "AGE")
+age.LOS
+
+group.TOTCHG <- ggplot(data, aes(APRDRG, TOTCHG, fill=APRDRG)) +
+  geom_bar(stat="identity", show.legend = F) +
+  xlim(600,650) + 
+  labs(y = "Total Charges", x='Group')
+group.TOTCHG
+
+group.LOS <- ggplot(data, aes(APRDRG, LOS, fill=APRDRG)) +
+  geom_bar(stat="identity", show.legend = T) + 
+  xlim(600,650) +
+  scale_fill_binned(name = "Group") +
+  labs(x='Group')
+group.LOS
+
+age.totchg.bp <- ggplot(data, aes(factor(AGE), TOTCHG)) + 
+  geom_boxplot(aes(fill ='#ff6668'), show.legend = F) + 
+  coord_flip() + 
+  theme_minimal() +
+  labs(y='Total Charges', x='Age')
+age.totchg.bp
+grid.arrange(arrangeGrob(age.TOTCHG, age.LOS,
+                         group.TOTCHG, group.LOS, ncol = 2),
+             age.totchg.bp, nrow=1)
+
+
+totch.female.bp <- ggplot(data, aes(factor(FEMALE.factored), TOTCHG)) + 
+  geom_boxplot(aes(fill ='#ff6668'), show.legend = F) +
+  coord_flip() +
+  labs(x='', y='Total Charges')
+totch.female.bp
+
+RACE.factored <- as.factor(data$RACE)
+race.totchg.bp <- ggplot(data, aes(factor(RACE.factored), TOTCHG)) + 
+  geom_boxplot(aes(fill ='#ff6668'), show.legend = F) +
+  coord_flip() +
+  labs(x='Race Groups', y='Total Charges')
+race.totchg.bp
+
+grid.arrange(totch.female.bp, race.totchg.bp, nrow=1)
 
